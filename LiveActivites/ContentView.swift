26500059dfb9fff7f89ff89fb1f0054ee97b5198
wrozier <williamrozier@icloud.com>
2023-10-22 12:30:43 -0400

    Initial Commit

---
+++ LiveActivites/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActivites
+//
+//  Created by Will Rozier on 10/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveActivites/LiveActivitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitesApp.swift
+//  LiveActivites
+//
+//  Created by Will Rozier on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
